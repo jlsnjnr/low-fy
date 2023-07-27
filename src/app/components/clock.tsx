4417b749845a6d1ad4f86d85b34448590af86af8
Joelson Júnior <contato.joelsonjunior@gmail.com>
2023-07-27 13:22:03 -0300

    first commit

---
+++ src/app/components/clock.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+
+const Time = dynamic(() => import('./time'), { ssr: false })
+
+type Props = {
+  time: number
+}
+
+export const Clock = ({ time: initial }: Props) => {
+  const [time, setTime] = useState(new Date(initial))
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  return <Time time={time} />
+}
