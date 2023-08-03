aca457481a01f7ba41d574fe9983e7eea11cf25d
Joelson Júnior <contato.joelsonjunior@gmail.com>
2023-08-02 21:22:57 -0300

    feat: bottom

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   const now = new Date();
 
   return (
-    <section className="flex items-center gap-6 mt-5 md:justify-between">
+    <section className="flex items-center px-5 gap-3 md:gap-3 mt-12 md:justify-between">
       <div className="w-16 h-8 flex items-center justify-center bg-[hsla(0,0%,7%,.75)] rounded-md">
         <Clock time={now.getTime()} />
       </div>
